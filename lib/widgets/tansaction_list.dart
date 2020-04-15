d6b5d5f574c0aeaf8d18b799ca3fde318c101578
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-04-15 15:09:05 +0100

    Feat: Create add transaction widget

--- lib/widgets/tansaction_list.dart
+++ lib/widgets/tansaction_list.dart
@@ -3,65 +3,81 @@ import 'package:flutter/material.dart';
 
 import 'package:intl/intl.dart';
 
-class TransactionList extends StatefulWidget {
-  @override
-  _TransactionListState createState() => _TransactionListState();
-}
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function _deleteTransactionHandler;
 
-class _TransactionListState extends State<TransactionList> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-        id: 't1', title: 'New shoes', amount: 69.44, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Airtime purchase', amount: 500, date: DateTime.now())
-  ];
+  TransactionList(this.transactions, this._deleteTransactionHandler);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: _userTransactions.map((tranx) {
-      return Card(
-        child: Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.purple, width: 2)),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                '\$${tranx.amount.toString()}',
-                style: TextStyle(
-                  color: Colors.purple,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 500,
+      child: transactions.isEmpty
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(
-                  tranx.title,
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+                  'No transaction added yet.',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
                 ),
-                Text(
-                  DateFormat().format(tranx.date),
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.blueGrey,
+                // Size box are invisibe but still ocupies space
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 100,
+                  child: Image.asset(
+                    'assets/images/shit.png',
+                    fit: BoxFit.cover,
                   ),
                 )
               ],
             )
-          ],
-        ),
-      );
-    }).toList());
+          : ListView.builder(
+              itemBuilder: (contex, index) {
+                return Card(
+                  elevation: 1,
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 50,
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: FittedBox(
+                          child: Text(
+                            '\$${transactions[index].amount}',
+                          ),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    subtitle: Text(
+                      DateFormat().format(transactions[index].date),
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        color: Theme.of(context).primaryColor,
+                        onPressed: () {
+                          _deleteTransactionHandler(transactions[index].id);
+                        }),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
+            ),
+    );
   }
 }
